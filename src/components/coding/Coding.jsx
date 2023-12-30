e9aad2c9ab5a232378008b02258d48efb98b7eef
Shreya Maurya <100403765+shreyamaurya029@users.noreply.github.com>
2023-12-30 23:55:47 +0530

    Update Coding.jsx

--- src/components/coding/Coding.jsx
+++ src/components/coding/Coding.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+
+import React from "react";
+import "./Coding.scss";
 
 const Coding = () => {
+  const leetCodeProfileLink = "https://leetcode.com/not_so_dark/";
+  const gfgProfileLink = "https://auth.geeksforgeeks.org/user/shreya21ug";
+
   return (
-    <div className='Coding'>
-        <div className="titleContainer">
-
+    <div className="coding-profiles-section">
+      <div className="profile-container">
+        <div className="profile">
+          <a
+            href={leetCodeProfileLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="leetcode.png"
+              alt="LeetCode Profile"
+              className="profile-image"
+            />
+          </a>
         </div>
 
-        <div className="listContainer">
-            
+        <div className="profile">
+          <a href={gfgProfileLink} target="_blank" rel="noopener noreferrer">
+            <img
+              src="gfg.jpeg"
+              alt="GeeksforGeeks Profile"
+              className="profile-image"
+            />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Coding
+export default Coding;
