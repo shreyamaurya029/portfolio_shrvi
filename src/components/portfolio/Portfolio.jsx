6b62604a9813ca6530237b9d11ede45539e928a8
Shreya Maurya <100403765+shreyamaurya029@users.noreply.github.com>
2024-01-14 00:08:29 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,21 +7,24 @@ const items = [
     id: 1,
     title: "Rental System",
     img: "https://github.com/shreyamaurya029/rental-system/blob/main/Screenshot%202023-12-27%20at%2012.39.19%20PM.png?raw=true",
-    desc: "FreeWheelRentals meets the demand for short-term vehicle rentals, particularly among college students. The platform enables users to easily reserve bicycles and motorbikes for city commuting or tours. With a user-friendly interface offering login, registration, and a contact section,it simplifies the booking process and addresses user inquiries",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsum natus animi temporibus esse ut, praesentium commodi. Tempore, maxime vero. Vel dolor odit ea tempore possimus deserunt modi nostrum error.",
+    link: "https://github.com/shreyamaurya029/rental-system",
   },
 
   {
     id: 2,
     title: "Seedling",
     img: "https://github.com/shreyamaurya029/_seedling_react/assets/100403765/b9583313-d993-4bc3-b96e-97b6e7dfdd74",
-    desc: "Seedling is a web application that aims to raise awareness about the importance of planting trees and provide information about various tree species. The project also offers a platform for users to contribute to tree planting efforts by donating money to NGOs",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsum natus animi temporibus esse ut, praesentium commodi. Tempore, maxime vero. Vel dolor odit ea tempore possimus deserunt modi nostrum error.",
+    link:"https://github.com/shreyamaurya029/_seedling_react",
   },
 
   {
     id: 3,
     title: "Memory Game",
     img: "https://github.com/shreyamaurya029/memory-game/blob/main/ss.png?raw=true",
-    desc: "This is a simple memory game built using React, where players test their memory by flipping cards and trying to find matching pairs",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsum natus animi temporibus esse ut, praesentium commodi. Tempore, maxime vero. Vel dolor odit ea tempore possimus deserunt modi nostrum error.",
+    link:"https://github.com/shreyamaurya029/memory-game",
   },
 ];
 
@@ -44,7 +47,9 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <button>See Demo</button>
+            </a>
           </motion.div>
         </div>
       </div>
