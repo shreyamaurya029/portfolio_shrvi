819bdafd9ba3f4771ad496db6693d08fc19570c8
Shreya Maurya <100403765+shreyamaurya029@users.noreply.github.com>
2024-01-13 23:55:37 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,21 +7,21 @@ const items = [
     id: 1,
     title: "Rental System",
     img: "https://github.com/shreyamaurya029/rental-system/blob/main/Screenshot%202023-12-27%20at%2012.39.19%20PM.png?raw=true",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsum natus animi temporibus esse ut, praesentium commodi. Tempore, maxime vero. Vel dolor odit ea tempore possimus deserunt modi nostrum error.",
+    desc: "FreeWheelRentals meets the demand for short-term vehicle rentals, particularly among college students. The platform enables users to easily reserve bicycles and motorbikes for city commuting or tours. With a user-friendly interface offering login, registration, and a contact section,it simplifies the booking process and addresses user inquiries",
   },
 
   {
     id: 2,
     title: "Seedling",
     img: "https://github.com/shreyamaurya029/_seedling_react/assets/100403765/b9583313-d993-4bc3-b96e-97b6e7dfdd74",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsum natus animi temporibus esse ut, praesentium commodi. Tempore, maxime vero. Vel dolor odit ea tempore possimus deserunt modi nostrum error.",
+    desc: "Seedling is a web application that aims to raise awareness about the importance of planting trees and provide information about various tree species. The project also offers a platform for users to contribute to tree planting efforts by donating money to NGOs",
   },
 
   {
     id: 3,
     title: "Memory Game",
     img: "https://github.com/shreyamaurya029/memory-game/blob/main/ss.png?raw=true",
-    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsum natus animi temporibus esse ut, praesentium commodi. Tempore, maxime vero. Vel dolor odit ea tempore possimus deserunt modi nostrum error.",
+    desc: "This is a simple memory game built using React, where players test their memory by flipping cards and trying to find matching pairs",
   },
 ];
 
